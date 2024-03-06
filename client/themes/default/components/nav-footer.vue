49065fba0a271b1d4ece580ad64aaa9de6fe10f6
spollacs <54198789+spollacs@users.noreply.github.com>
2024-03-07 01:27:18 +0800

    Update nav-footer.vue

--- client/themes/default/components/nav-footer.vue
+++ client/themes/default/components/nav-footer.vue
@@ -5,8 +5,7 @@
         span(v-html='footerOverrideRender + ` |&nbsp;`')
       template(v-else-if='company && company.length > 0 && contentLicense !== ``')
         span(v-if='contentLicense === `alr`') {{ $t('common:footer.copyright', { company: company, year: currentYear, interpolation: { escapeValue: false } }) }} |&nbsp;
-        span(v-else) {{ $t('common:footer.license', { company: company, license: $t('common:license.' + contentLicense), interpolation: { escapeValue: false } }) }} |&nbsp;
-      span {{ $t('common:footer.poweredBy') }} #[a(href='https://wiki.js.org', ref='nofollow') Wiki.js]
+        span(v-else) {{ $t('common:footer.license', { company: company, license: $t('common:license.' + contentLicense), interpolation: { escapeValue: false } }) }} 
 </template>
 
 <script>
